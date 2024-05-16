cf37d1d0d057869ce525348804780369c3b46813
Jui1 <juimandal31@gmail.com>
2024-05-17 01:47:53 +0530

    first commit

---
+++ props/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Card from './compoents/card'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+
+   <Card/>
+    </>
+  )
+}
+
+export default App
